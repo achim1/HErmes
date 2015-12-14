fc97a2a0926cde2db8952af3f75db3b5b8ff92db
Achim Stoessl <achim.stoessl@gmail.com>
2015-12-14 13:40:40 -0600

    * fixes example

--- examples/grab_l2_files.py
+++ examples/grab_l2_files.py
@@ -1,7 +1,7 @@
 import utils.files as f
 
-all = f.harvest_files("/data/exp/IceCube/2015/filtered/level2/1127",prefix=None,ending=".bz2")
-gcd = f.harvest_files("/data/exp/IceCube/2015/filtered/level2/",prefix=None,ending=".gz")
+all = f.harvest_files("/data/exp/IceCube/2015/filtered/level2/1127",prefix="",ending=".bz2")
+gcd = f.harvest_files("/data/exp/IceCube/2015/filtered/level2/VerifiedGCD",prefix="",ending=".gz")
 all_w_gcd = all + gcd
 
 runs = f.group_names_by_regex(all_w_gcd,firstpattern=f.GCD,estimate_first= lambda x : sorted(x)[1])
@@ -9,7 +9,7 @@ runs = f.group_names_by_regex(all_w_gcd,firstpattern=f.GCD,estimate_first= lambd
 #remove runs which have only gcd
 runs_on_1127 = dict()
 for k in runs.keys():
-    if len(runs[k] > 1):
+    if len(runs[k]) > 1:
         runs_on_1127[k] = runs[k]
 
 print runs_on_1127

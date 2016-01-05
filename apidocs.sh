2fc9b16519b5af874e0f058d19d7680cad4ef7ed
Achim Stoessl <achim.stoessl@gmail.com>
2016-01-05 15:33:10 -0600

    * one step further to jekyll

--- apidocs.sh
+++ apidocs.sh
@@ -5,3 +5,6 @@ rm -rf docs/build/doctrees
 rm docs/build/Gemfile
 echo "source 'https://rubygems.org'" > docs/build/Gemfile
 echo "gem 'github-pages'" >> docs/build/Gemfile
+mv docs/build/html docs/build/apidocs
+cp -r docs/jekyll-docs/* docs/build
+ 

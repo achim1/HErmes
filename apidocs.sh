fa4dd443f5eef0049aebf060f1573fad41c9ea6a
Achim Stoessl <achim.stoessl@gmail.com>
2016-04-18 23:11:09 -0400

    * fixes apidoc create script

--- apidocs.sh
+++ apidocs.sh
@@ -5,8 +5,11 @@ rm -rf docs/build/doctrees
 rm docs/build/Gemfile
 echo "source 'https://rubygems.org'" > docs/build/Gemfile
 echo "gem 'github-pages'" >> docs/build/Gemfile
-rm docs/build/apidocs/*
-mv docs/build/html/* docs/build/apidocs
-rmdir docs/build/html
+rm -rf docs/build/apidocs
+mkdir docs/build/apidocs
+cp -r docs/build/html/* docs/build/apidocs
+#rm docs/apidocs/*
+#mv docs/build/html/* docs/apidocs
+#rmdir docs/build/html
 cp -r docs/jekyll-docs/* docs/build
  

2dbe8e19e0ea5fd8979daf33fccaef20bd2c6d45
Achim Stoessl <achim.stoessl@gmail.com>
2017-03-27 16:53:14 +0900

    * adds tests

---
+++ tests/test_icecubegoodies.py
@@ -0,0 +1,17 @@
+import pytest
+import numpy as np
+
+import pyevsel.icecube_goodies.helpers as helpers
+
+def test_geometry():
+    geo = helpers.IceCubeGeometry()
+    for string in range(1,86):
+        print (string)
+        for dom in range(1,61):
+            c =  geo.coordinates(string, dom)
+            assert len(c) == 3
+            for k in c:
+                assert np.isfinite(k)
+
+
+

* bugfixes: fixes a bug in the models chi2 calculation             fixes another bug in Datasets
* introduces the density parameter for normalization of model data

pyevsel/fitting/functions.py
@@ -12,7 +12,7 @@ from future import standard_library
 standard_library.install_aliases()
 
 
-def poisson(lmbda, k):
+def poisson(k, lmbda):
     """
     Poisson distribution
 
@@ -28,7 +28,7 @@ def poisson(lmbda, k):
 
 ################################################
 
-def gauss(x, mu, sigma, n):
+def gauss(x, mu, sigma):
     """
     Returns a normed gaussian.
 
@@ -41,6 +41,32 @@ def gauss(x, mu, sigma, n):
     Returns:
 
     """
+
+    def _gaussnorm(sigma):
+        return 1 / (sigma * np.sqrt(2 * np.pi))
+
+    return _gaussnorm(sigma) * np.exp(-np.power((x - ( mu)), 2) / (2 *  (sigma ** 2)))
+
+
+
+################################################
+
+def n_gauss(x, mu, sigma, n):
+    """
+    Returns a normed gaussian in the case of n ==1. If n > 1, The gaussian
+    mean is shifted by n and its width is enlarged by the factor of n.
+    The envelope of a sequence of these gaussians will be an expoenential.
+
+    Args:
+        x (np.ndarray): x values
+        mu (float): Gauss mu
+        sigma (float): Gauss sigma
+        n (int): > 0, linear coefficient
+
+    Returns:
+
+    """
+    assert n > 0, "Can not compute gauss with n <= 0"
 
     def _gaussnorm(sigma, n):
         return 1 / (sigma * np.sqrt(2 * n * np.pi))

pyevsel/fitting/model.py
@@ -95,7 +95,7 @@ class Model(object):
         """
         return sum(self.n_free_params)
 
-    def _create_distribution(self,data, nbins, normalize=False):
+    def _create_distribution(self,data, nbins, normalize=False, density=True):
         """
         Create a distribution
 
@@ -109,7 +109,7 @@ class Model(object):
 
         self.norm = 1
         if normalize:
-            h_norm = h.normalized(density=True)
+            h_norm = h.normalized(density=density)
             norm = h.bincontent / h_norm.bincontent
             norm = norm[np.isfinite(norm)][0]
             self.norm = norm
@@ -244,6 +244,7 @@ class Model(object):
     def add_data(self, data, nbins=200,\
                  create_distribution=False,\
                  normalize=False,\
+                 density=True,\
                  xs=None,\
                  subtract=None):
         """
@@ -257,12 +258,13 @@ class Model(object):
             nbins (int):
             subtract (callable):
             normalize (bool): normalize the data before adding
-
+            density (bool): if normalized, assume the data is a pdf.
+                            if False, use bincount for normalization.
         Returns:
 
         """
         if create_distribution:
-            self._create_distribution(data, nbins, normalize)
+            self._create_distribution(data, nbins, normalize, density=density)
             self.ndf = nbins - len(self.startparams)
         else:
             assert xs is not None, "Have to give xs if not histogramming!" 
@@ -334,7 +336,7 @@ class Model(object):
         #    norm = norm[np.isfinite(norm)][0]
 
         #self.norm = norm
-        chi2 = (funcs.calculate_chi_square(self.data, self.norm * model(self.xs, *parameters)))
+        chi2 = (funcs.calculate_chi_square(self.norm*self.data, self.norm * model(self.xs, *parameters)))
         self.chi2_ndf = chi2/self.ndf
 
         # FIXME: new feature

tests/test_fitting.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 from pyevsel.fitting import fit, model, functions
 
@@ -123,10 +124,39 @@ def test_model_plot_result_distribution():
     model.fit_to_data()
     assert isinstance(model.plot_result(), p.Figure)
 
-    
 def test_model_clear():
     model = modelfactory()
     newmodel = model
     model.clear()
     assert model == newmodel
+
+def test_gauss():
+    assert functions.gauss(1, 0, .2) == functions.n_gauss(1, 0, .2, 1)
+
+    data = np.random.normal(0, .2, 10000)
+    gaussmod = model.Model(functions.gauss)
+
+    gaussmod.startparams = [-1, 1]
+    gaussmod.add_data(data, create_distribution=True, normalize=True)
+    assert gaussmod.ndf == 198
+
+    gaussmod.fit_to_data()
+    assert 0.7 < gaussmod.chi2_ndf < 1.2
+    assert -0.1 < gaussmod.best_fit_params[0] < .1
+    assert .18 < gaussmod.best_fit_params[1] < .22
+
+def test_poisson():
+
+    data = np.random.poisson(100, size=10000)
+
+    mod = model.Model(functions.poisson)
+
+    mod.startparams = [80]
+    mod.add_data(data, create_distribution=True, normalize=True, density=False)
+    assert mod.ndf == 199
+
+    mod.fit_to_data()
+    assert 0.5 < mod.chi2_ndf < 1.2
+    assert 90 < mod.best_fit_params[0] < 110
+
 
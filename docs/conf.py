679d68e16cf6f05de79ca63973b8b45ed053e1ad
Achim Stoessl <achim.stoessl@gmail.com>
2017-09-11 17:06:51 +0900

    * adds mock module to conf.py for Readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,20 @@ import os
 import sys
 import sphinx_rtd_theme
 # sys.path.insert(0, os.path.abspath('.'))
+import mock
+ 
+MOCK_MODULES = ['numpy',\
+                'scipy',\
+                'matplotlib',\
+                'matplotlib.pyplot',\
+                'scipy.interpolate',\
+                'iminuit',\
+                'tables',\
+                'pandas',
+                'seaborn']
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 
 # -- General configuration ------------------------------------------------

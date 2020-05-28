5cae9b933766e490e884b91156e7f10a7a16853f
Achim Stoessl <achim.stoessl@gmail.com>
2020-05-28 08:17:19 -1000

    FEATURE - ups version number

--- HErmes/__init__.py
+++ HErmes/__init__.py
@@ -19,7 +19,7 @@ The HErmes modules provides the following submodules:
 
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ["fitting", "icecube_goodies", "utils",\
            "selection", "visual", "analysis"]
 
